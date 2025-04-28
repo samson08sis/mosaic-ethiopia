cd150e698f897fda5f26e30e9b78dfb0bfd6dee0
Samson <sams1307wolde@gmial.com>
2025-04-28 19:58:37 +0300

    changed copy icon

--- components/ui/CopyButton.tsx
+++ components/ui/CopyButton.tsx
@@ -1,5 +1,5 @@
 import { useCopy } from "@/utils/copy-util";
-import { useState } from "react";
+import { Copy, CopyCheck } from "lucide-react";
 
 interface CopyButtonProps {
   textToCopy: string;
@@ -37,18 +37,9 @@ export default function CopyButton({ textToCopy }: CopyButtonProps) {
           />
         </svg>
       ) : (
-        <svg
-          className="w-4 h-4 opacity-70 hover:opacity-100 transition-opacity"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
-          />
-        </svg>
+        <Copy
+          className={`h-4 w-4 opacity-70 hover:opacity-100 transition-opacity duration-300`}
+        />
       )}
     </button>
   );
