529b8ab1bfc6d44d985c50ac48c351eac96613ab
Samson <sams1307wolde@gmail.com>
2025-06-15 04:23:51 +0300

    Implemented real forgot password request functionality

--- app/(auth)/forgot-password/page.tsx
+++ app/(auth)/forgot-password/page.tsx
@@ -35,14 +35,14 @@ export default function ForgotPasswordPage() {
     }
 
     try {
-      // Simulate API call
-      // await new Promise((resolve) => setTimeout(resolve, 1000));
-
-      await fetch("http://localhost:5000/api/auth/forgot-password", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email }),
-      });
+      await fetch(
+        "https://mosaic-backend-li68.vercel.app/api/auth/forgot-password",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email }),
+        }
+      );
 
       setIsSubmitted(true);
     } catch (err) {
