b9ebcdc0869eae0fbf17e66c6fa3e0d67586df06
Samson <sams1307wolde@gmail.com>
2025-07-19 22:44:03 +0300

    Updated form input component

--- app/(auth)/forgot-password/page.tsx
+++ app/(auth)/forgot-password/page.tsx
@@ -101,7 +101,9 @@ export default function ForgotPasswordPage() {
                     onChange={(e) => setEmail(e.target.value)}
                     placeholder="you@example.com"
                     required
-                    icon={<Mail className="h-5 w-5" />}
+                    icon={
+                      <Mail className="absolute left-3 top-3.5 h-5 w-5 text-gray-400" />
+                    }
                   />
 
                   <button
