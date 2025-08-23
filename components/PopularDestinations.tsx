806734f202c4e101803ca7143bb52c5ad440d88b
Samson <sams1307wolde@gmail.com>
2025-08-24 02:41:11 +0300

    Upated Home content to use ISR

--- components/PopularDestinations.tsx
+++ components/PopularDestinations.tsx
@@ -1,49 +1,28 @@
-"use client";
+import { MinimalDestination } from "@/types/destinations/types";
+import PopularDestinationCard from "./home/PopularDestinationCard";
 
-import { useLanguage } from "@/contexts/LanguageContext";
-import Link from "next/link";
-import StarIcon from "./ui/svgs/StarSVG";
-// import destinations from "@/data/destinations";
+const getPopularDestinations = async () => {
+  const res = await fetch(
+    `${process.env.BACKEND_URL}/api/destinations/popular`,
+    {
+      next: { revalidate: 21600 },
+      cache: "force-cache",
+    }
+  );
+  if (!res.ok) throw new Error("Failed to fetch destination");
+  const data = await res.json();
+  return data;
+};
 
-export default function PopularDestinations() {
-  const { translations } = useLanguage();
-
-  const destinations = [
-    {
-      id: "lalibela",
-      name: "Lalibela, Ethiopia",
-      image: "/lalibela-bete-giorgis.jpg",
-      rating: 4.9,
-      reviews: 245,
-    },
-    {
-      id: "simien-mountains",
-      name: "Simien Mountains, Ethiopia",
-      image: "/bg-2.jpg",
-      rating: 4.8,
-      reviews: 187,
-    },
-    {
-      id: "danakil",
-      name: "Danakil Depression, Ethiopia",
-      image: "/bg-53.jpg",
-      rating: 4.7,
-      reviews: 156,
-    },
-    {
-      id: "gondar",
-      name: "Gondar, Ethiopia",
-      image: "/bg-54.jpg",
-      rating: 4.6,
-      reviews: 132,
-    },
-  ];
-
+export default async function PopularDestinations() {
+  const destinations: MinimalDestination[] = await getPopularDestinations();
   return (
     <section id="destinations" className="pb-16">
       <div className="text-center mb-12">
         <h2 className="text-3xl font-bold mb-4 text-gray-900 dark:text-white">
-          {translations.popularDestinations || "Popular Destinations"}
+          {/* FIX LATER TRANSLATIONS */}
+          {/* {translations.popularDestinations || "Popular Destinations"} */}
+          Popular Destinations
         </h2>
         <p className="max-w-2xl mx-auto text-gray-600 dark:text-gray-300">
           Explore our most sought-after destinations loved by travelers around
@@ -53,45 +32,7 @@ export default function PopularDestinations() {
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
         {destinations.map((destination) => (
-          <div
-            key={destination.id}
-            className="group rounded-xl overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300 bg-white dark:bg-gray-800">
-            <div className="relative h-64 overflow-hidden">
-              <img
-                src={
-                  destination.image || "/placeholder.svg?height=300&width=400"
-                }
-                alt={destination.name}
-                className="w-full h-full object-cover transform group-hover:scale-110 transition-transform duration-300"
-              />
-              <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
-              <div className="absolute bottom-4 left-4 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                <span className="flex items-center">
-                  <StarIcon customStyle="text-yellow-400" />
-                  <span className="ml-1">
-                    {destination.rating} ({destination.reviews})
-                  </span>
-                </span>
-              </div>
-            </div>
-            <div className="p-4">
-              <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                {destination.name}
-              </h3>
-              <div className="mt-4 flex flex-col xl:flex-row gap-2">
-                <Link
-                  href={`/destinations/${destination.id}`}
-                  className="w-full px-4 py-2 bg-primary-600 hover:bg-primary-700 text-white rounded-lg transition-colors">
-                  View Details
-                </Link>
-                <Link
-                  href={`/book?destination=${destination.id}`}
-                  className="w-full px-4 py-2 border border-primary text-primary rounded-lg hover:bg-primary-50 transition-colors">
-                  Book Now
-                </Link>
-              </div>
-            </div>
-          </div>
+          <PopularDestinationCard destination={destination} />
         ))}
       </div>
     </section>
---
+++ components/home/PopularDestinationCard.tsx
@@ -0,0 +1,61 @@
+import { MinimalDestination } from "@/types/destinations/types";
+import StarIcon from "../ui/svgs/StarSVG";
+import { Tag } from "lucide-react";
+import Link from "next/link";
+
+export default function PopularDestinationCard({
+  destination,
+}: {
+  destination: MinimalDestination;
+}) {
+  return (
+    <div
+      // ****************************** FIX LATER ********************************* // => key = id
+      key={destination.name}
+      className="group rounded-xl overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300 bg-white dark:bg-gray-800">
+      <div className="relative h-64 overflow-hidden">
+        <img
+          src={destination.image || "/placeholder.svg?height=300&width=400"}
+          alt={destination.name}
+          className="w-full h-full object-cover transform group-hover:scale-110 transition-transform duration-300"
+        />
+        <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+        <div className="absolute bottom-4 left-4 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+          <span className="flex items-center">
+            <StarIcon customStyle="text-yellow-400" />
+            <span className="ml-1">
+              {destination.rating} ({destination.reviews})
+            </span>
+          </span>
+        </div>
+      </div>
+      <div className="flex flex-wrap gap-2 mt-4 px-4">
+        {destination.activities.map((activity) => (
+          <span
+            key={activity}
+            className="bg-muted px-3 py-1 rounded-full text-xs flex flex-row">
+            <Tag size={15} className="mr-2 dark:text-cyan-300" />
+            {activity}
+          </span>
+        ))}
+      </div>
+      <div className="p-4">
+        <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+          {destination.name}
+        </h3>
+        <div className="mt-4 flex flex-col xl:flex-row gap-2">
+          <Link
+            href={`/destinations/${destination.id}`}
+            className="w-full px-4 py-2 bg-primary-600 hover:bg-primary-700 text-white rounded-lg transition-colors">
+            View Details
+          </Link>
+          <Link
+            href={`/book?destination=${destination.id}`}
+            className="w-full px-4 py-2 border border-primary text-primary rounded-lg hover:bg-primary-50 transition-colors">
+            Book Now
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
