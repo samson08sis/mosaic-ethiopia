d5636330184d3764a712afb745e513205f29f7e7
Samson <sams1307wolde@gmail.com>
2025-08-24 03:05:02 +0300

    Created a Modal context to make modal management more organized

---
+++ contexts/ModalContext.tsx
@@ -0,0 +1,21 @@
+import { createContext, ReactNode, useContext, useState } from "react";
+
+const ModalContext = createContext({
+  isOpen: false,
+  openModal: () => {},
+  closeModal: () => {},
+});
+
+export const ModalProvider = ({ children }: { children: ReactNode }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+
+  return (
+    <ModalContext.Provider value={{ isOpen, openModal, closeModal }}>
+      {children}
+    </ModalContext.Provider>
+  );
+};
+
+export const useModal = () => useContext(ModalContext);
