b684d73249df5823d659ef3858cf1fad5bd32336
Samson <sams1307wolde@gmail.com>
2025-08-30 15:18:34 +0300

    Updated revalidator to use dynamic tags passed in the body instead

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -3,7 +3,7 @@ import { revalidatePath, revalidateTag } from "next/cache";
 
 export async function POST(request: Request) {
   const body = await request.json();
-  const secret = body.secret;
+  const { secret, tag } = body;
 
   if (secret !== process.env.REVALIDATION_SECRET) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
@@ -11,7 +11,7 @@ export async function POST(request: Request) {
 
   try {
     // revalidatePath("/"); // Revalidate home page
-    revalidateTag("hero-section"); // Revalidate Hero section
+    revalidateTag(tag); // Revalidate the fetches tagged with the passed tag
     return NextResponse.json({ revalidated: true });
   } catch (err: any) {
     return NextResponse.json(
